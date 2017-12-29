caf854534cb5edc8935cf964a15ea4c8a88fa3c3
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2017-12-29 12:52:24 +0900

    Update to my personal info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    author: 'Kyle Mathews',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
+    title: 'メモログ',
+    author: 'Yutaka Yamaguchi',
+    description: 'Just my personal notes',
+    siteUrl: 'https://memolog.github.io/site',
   },
   pathPrefix: '/site',
   plugins: [
